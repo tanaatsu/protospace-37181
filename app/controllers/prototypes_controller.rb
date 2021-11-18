9fd210ccaade09f14da67ab2be26a76412e8bef0
tanaatsu <atsushi19890825@gmail.com>
2021-11-18 15:14:46 +0900

    エラー修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -21,6 +21,7 @@ class PrototypesController < ApplicationController
 
   def show
     @prototype = Prototype.find(params[:id])
+    
     @comment = Comment.new
     @comments = @prototype.comments.includes(:user)
   end
